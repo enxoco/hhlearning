f1993a83ecdfdc108b940bc9f60bb5ea7833e600
enxoco <mike@enxo.co>
2022-07-19 19:36:01 -0400

    Moving frontend into mono repo

--- frontend/src/components/elements/Card.tsx
+++
@@ -1,6 +0,0 @@
-import { Box } from "@chakra-ui/layout"
-
-export const Card: React.FC<CardProps> = ({children, ...props}) => {
-
-    return (<Box borderRadius={5} borderWidth={2} p={5} mt={20} minW={350} shadow="md" {...props}>{children}</Box>)
-}
