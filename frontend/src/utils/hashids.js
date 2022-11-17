c3dfa40c272044d28e5d470bc3eb5c31cfe0f9b2
Mike Conrad <mike@enxo.co>
2022-11-17 12:40:16 -0500

    Mount the dockerConfig folder into the db container and fix frontend bugs. This commit fixes an issue where hashids was not recieving env variables due to the fact that they were prefixed with react instead of vite. Also resolved an issue where the incorrect hashed id was being used in the student table and on the edit courses page.

--- frontend/src/utils/hashids.js
+++ frontend/src/utils/hashids.js
@@ -1,2 +1,2 @@
 import Hashids from 'hashids'
-export const hashids = new Hashids(import.meta.env.REACT_APP_SALT, import.meta.env.REACT_APP_SALT_LENGTH)
+export const hashids = new Hashids(import.meta.env.VITE_APP_SALT, +import.meta.env.VITE_APP_SALT_LENGTH)
